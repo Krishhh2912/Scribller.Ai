86c681556f2d1abb44dac1f6a5dd60e99044ddf1
Krishhh2912 <95735363+Krishhh2912@users.noreply.github.com>
2024-04-16 09:59:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { loadFull } from "tsparticles";
 import "./App.css";
 import particlesOptions from "./particles.json";
 
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 function App() {
-
   const [init, setInit] = useState(false);
   const [userInput, setUserInput] = useState('');
   const [generatedImage, setGeneratedImage] = useState(null);
@@ -28,7 +28,7 @@ function App() {
     const response = await fetch(
       "https://api-inference.huggingface.co/models/MdEndan/stable-diffusion-lora-fine-tuned",
       {
-        headers: { Authorization: "Bearer xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" },
+        headers: { Authorization: "Bearer ${ACCESS_TOKEN}" },
         method: "POST",
         body: JSON.stringify(data),
       }
